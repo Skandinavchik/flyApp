34d0c14931dfa0e36ded6ce6857a7010cdeb071e
Artem Sokolov <sknd2569@gmail.com>
2023-06-09 17:02:06 +0200

    flightController updated

--- src/controllers/flightsController.js
+++ src/controllers/flightsController.js
@@ -3,7 +3,45 @@ import { Flight } from "../models/flightsModel.js";
 
 const getAllFlights = async (req, res) => {
     try {
-        const flights = await Flight.find();
+
+        const { from, to, sort } = req.query;
+
+        const queryObject = { ...req.query };
+        const excludedFields = ['limit', 'page', 'sort'];
+        excludedFields.forEach(item => delete queryObject[item]);
+
+        let query = Flight.find(from && to
+            ? { 'from': { $regex: `${from}`, $options: 'i' }, 'to': { $regex: `${to}`, $options: 'i' } }
+            : queryObject);
+
+        from
+            ? query = query.find({ 'from': { $regex: `${from}`, $options: 'i' } })
+            : queryObject;
+
+        sort
+            ? query = query.sort(sort.replaceAll(',', ' '))
+            : query = query.sort('from');
+
+        const page = +req.query.page || 1;
+        const limit = +req.query.limit || 20;
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
+
+        if (req.query.page) {
+            const flightsAmount = await Flight.countDocuments();
+
+            if (skip >= flightsAmount) {
+                res.status(404).json({
+                    status: 'failed',
+                    message: `This page doesn't exist`
+                });
+                return;
+            }
+        }
+
+        // Execute query
+        const flights = await query;
 
         res.status(200).json({
             status: 'success',
